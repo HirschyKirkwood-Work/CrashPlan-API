64c07a365319b91336b9db8cd18734ddb63a428e
HirschyKirkwood-Work <hkirkwoo@andrew.cmu.edu>
2022-10-18 09:37:16 -0400

    Added sed-like function to standardize input files with what is needed from them.

--- code42.py
+++ code42.py
@@ -6,19 +6,25 @@ from helper import *
 
 def main():
 	user_file = file_dia()
+	sed_inplace(user_file, r'^\"ldap\",', '')
+	sed_inplace(user_file, r'\"sourceId\",\"entityId\"\n', '')
+	sed_inplace(user_file, r'^$', '')
 	cp_all_users = get_users()
 	dept_members = import_users(user_file)
 	no_acc = no_account(dept_members, cp_all_users)
 	for user in dept_members:
+		print(user)
 		if user in no_acc:
-			print(f"No Account: {user}")
+			print(f"No Account: {user}\n")
+			continue
+		current = cp_all_users[user]
+		user_machine_status(current)
+		print("\n")
 
 
 if __name__ == '__main__':
-	#main()
-	for key,value in get_machines("f6286ad2531aea56").items():
-		print(f"Computer name: {key}\n"
-			f"Status: {value}\n")
+	main()
+
 
 
 
--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 import py42.sdk
-import csv
+import csv, re, shutil, tempfile
 
 
 dotenv_path = Path('creds.env')
@@ -56,18 +56,22 @@ def get_machines(UID):
 	    	# if 'OK' in str(device['alertStates']):
 	    	# 	continue
 	    	if device['userUid'] == UID:
-	    		user_devices[device['osHostname']] = device['alertStates']
+	    		user_devices[device['osHostname']] = { 'Alert_State': device['alertStates'],'Status': device['status'],
+	    		'UID': device['userUid'], 'Last_Modified': device['lastConnected'], 'Os_Type': device['osName']}
 	    		# return f"Hostname: {device['osHostname']}\nUserUID: {device['userUid']}\nOperating System: {device['osName']}\nAlert {device['alertStates']}\n"
 	    	#if 'OK' not in str(device['alertStates']):
 	    		#print(device['alertStates'])
 	    		#return f"Hostname: {device['osHostname']}\nUserID: {device['userId']}\nOperating System: {device['osName']}\nAlert {device['alertStates']}\n"
 	    return user_devices
+
+
 # {'userId': 28062831, 'userUid': '1060852660876723881', 'status': 'Active', 'username': 'alauth@andrew.cmu.edu', 
 #'email': 'alauth@andrew.cmu.edu', 'firstName': 'Aaron', 'lastName': 'Lauth', 'quotaInBytes': -1, 'orgId': 537140, 
 #'orgUid': 'ffdf5749-1d54-44e4-8f0e-da3c3c63e68e', 'orgGuid': '89muscpm892chp8c', 'orgName': 'Managed Hardware', 
 #'userExtRef': None, 'notes': None, 'active': True, 'blocked': False, 'invited': False, 'orgType': 'ENTERPRISE', 
 #'usernameIsAnEmail': True, 'creatdeptnDate': '2022-05-20T16:44:20.249Z', 'modificatdeptnDate': '2022-10-13T16:45:56.372Z', 
 #'passwordReset': False, 'localAuthenticatdeptnOnly': False, 'licenses': []}
+
 def get_users():
 	user_response = SDK.users.get_all(active=True)
 	all_users = {}
@@ -75,10 +79,6 @@ def get_users():
 		userz = page['users']
 		for user in userz:
 			all_users[user['username']] = user['userUid']
-			# if user['username'] not in dept:
-			# 	print(f"{user['username']}")
-			# with open("users.txt", "a+") as f:
-			# 	f.write(f"{user['username']}\n")
 	return all_users
 
 def no_account(department, all_users):
@@ -92,3 +92,36 @@ def file_dia():
 	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 	filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 	return filename
+
+def sed_inplace(filename, pattern, repl):
+    '''
+    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+    '''
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+    # writing with updating). This is usually a good thing. In this case,
+    # however, binary writing imposes non-trivial encoding constraints trivially
+    # resolved by switching to text writing. Let's do that.
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                tmp_file.write(pattern_compiled.sub(repl, line))
+
+    # Overwrite the original file with the munged temporary file in a
+    # manner preserving file attributes (e.g., permissions).
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, filename)
+
+
+def user_machine_status(UID):
+	#print(f"Length: {len(get_machines(UID).items())}")
+	if len(get_machines(UID).items()) != 0:
+		for key,value in get_machines(UID).items():
+			print(f"Computer name: {key}\n"
+				f"Status: {value['Status']},  Last_Modified: {value['Last_Modified'][0:10]}\n"
+				f"Alert: {value['Alert_State']}, OS: {value['Os_Type']}\n")
+	else:
+		print(f"User with UID {UID} does not have a machine associated with them.")

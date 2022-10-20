b388be07662c2983af52007537348cf36255ef42
HirschyKirkwood-Work <hkirkwoo@andrew.cmu.edu>
2022-10-19 21:02:59 -0400

    Adding comments for everything

--- code42.py
+++ code42.py
@@ -6,7 +6,7 @@ from helper import *
 #TODO users with accounts and bad backups (done).
 #TODO List all non-@andrew/alias emails (done).
 
-def get_grouper():
+def get_grouper(): # Recieves file from user, formats it as needed, get all CP users and returns values
 	user_file = file_dia()
 	sed(user_file, r'^\"ldap\",', '')
 	sed(user_file, r'\"sourceId\",\"entityId\"\n', '')
@@ -15,57 +15,52 @@ def get_grouper():
 	dept_members = import_users(user_file)
 	return user_file, cp_all_users, dept_members
 
-def main():
+def main(): # Rename eventually. This function returns the most info to user
 	user_file, cp_all_users, dept_members  = get_grouper()
-	dept_dict = return_dept_dict(cp_all_users, dept_members)
 	no_acc = no_account(dept_members, cp_all_users) # Almost removed this, but not using dept_dict() 
 	for member in dept_members:						# here removes the functionality of showing users w/o accounts, lol
-		if member in no_acc:
+		if member in no_acc: 
 			print(f"{member} does not have an CrashPlan account.\n")
 			continue
 		print(member)
-		user_machine_status(cp_all_users[member])
-	# for user,UID in dept_dict.items():
-	# 	print(user)
-	# 	user_machine_status(UID)
-	# 	print("\n")
+		user_machine_status(cp_all_users[member]) # Gets machine info for each machine associated with user.
 
-def no_backup():
+def no_backup(): # Checks for users with no backups
 	user_file, cp_all_users, dept_members  = get_grouper()
-	dept_dict = return_dept_dict(cp_all_users, dept_members)
-	count = 0
+	dept_dict = return_dept_dict(cp_all_users, dept_members) # Gets a dictionary of all users in Grouper that have an account.
+	count = 0 # Count of users w/ no backup.
 	for user in return_no_backup(dept_dict):
 		print(user)
 		count += 1
 	print(f"The number of users in this department without a backup is: {count}")
 
 
-def accountless():
-	user_file, cp_all_users, dept_members = get_grouper()
-	no_acc = no_account(dept_members, cp_all_users)
+def accountless(): # Gets All users with no account. Basically a wrapper function for no_account
+	user_file, cp_all_users, dept_members = get_grouper() # Gets necessary info
+	no_acc = no_account(dept_members, cp_all_users) # Gets users with no account
 	for user in dept_members:
 		if user in no_acc:
 			print(f"No Account: {user}\n")
 
 
-def aliases():
+def aliases(): # Gets all users without @andrew
 	cp_all_users = get_users()
-	count = 0
+	count = 0 # Count of Alias users
 	for user in cp_all_users:
-		if "andrew" not in user:
+		if "andrew" not in user: # Checks for Andrew
 			print(user)
-			count += 1
+			count += 1 
 	print(f"The amount of all Managed Hardware users who have an alias is {count}\n"
 		"This doesn't mean they don't have an actual email associated with their machines and should be checked manually.")
 
 if __name__ == '__main__':
-	print("Show users with no account based on grouper: 1\n"
+	print("Show users with no account based on grouper: 1\n" # Prints info
 		"Show users with an account but not a backup: 2\n"
 		"Combine 1 and 2: 3\n"
 		"Show users who don't have @andrew.cmu.edu: 4\no")
-	answer = input("1/2/3/4: ")
-	start_time = datetime.now()
-	if answer == "1":
+	answer = input("1/2/3/4: ") # Gets user input
+	start_time = datetime.now() # Beginning of timing. Done here so reading options isn't counted in. 
+	if answer == "1":  			# Unforunately no *easy* way of not including the time taken choosing a file as it's not in this function.
 		print("Checking for users with no account in CrashPlan")
 		accountless()
 	elif answer == "2":
@@ -80,7 +75,7 @@ if __name__ == '__main__':
 	else:
 		print("Invalid option.")
 	end_time = datetime.now()
-	print(f'Duration: {end_time - start_time}')
+	print(f'Duration: {end_time - start_time}') # Prints time taken
 	# accountless()
 	# no_backup()
 	# main()
--- helper.py
+++ helper.py
@@ -11,14 +11,14 @@ import csv, re, shutil, tempfile
 
 
 dotenv_path = Path('creds.env')
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv(dotenv_path=dotenv_path) # Loads creds from a file in the .gitignore.
 URL =  'https://console.us2.crashplan.com/api/v3/auth/jwt?useBody=true'
 UNAME = os.environ.get('username')
 PWORD =  os.environ.get('password')
-SDK = py42.sdk.from_local_account(URL, UNAME, PWORD)
+SDK = py42.sdk.from_local_account(URL, UNAME, PWORD) # Gets connection
 
 
-def flatten(l):
+def flatten(l): # Flattens out nexted list
 	return [item for sublist in l for item in sublist]
 
 def import_users(filename):
@@ -48,10 +48,10 @@ def get_machines(UID):
 				# 'creatdeptnDate': '2020-07-13T10:14:14.594Z','modificatdeptnDate': '2022-10-13T16:14:02.734Z',
 				# 'loginDate': '2022-02-16T15:13:59.460Z', 'service': 'CrashPlan'}
 
-	machine_response = SDK.devices.get_all(active=True, page_size=20000)
-	for page in machine_response:
+	machine_response = SDK.devices.get_all(active=True, page_size=20000) # Page size limits are stupid, but necessary, lol
+	for page in machine_response: # LOOOOOP
 	    devices = page["computers"]
-	    user_devices = {}
+	    user_devices = {} # Nested dict for user's devices
 	    for device in devices:
 	    	# if 'OK' in str(device['alertStates']):
 	    	# 	continue
@@ -73,34 +73,34 @@ def get_machines(UID):
 #'passwordReset': False, 'localAuthenticatdeptnOnly': False, 'licenses': []}
 
 def get_users():
-	user_response = SDK.users.get_all(active=True)
-	all_users = {}
+	user_response = SDK.users.get_all(active=True) # Could add page size here, but as of now, not required. Maybe DSP will get massive.
+	all_users = {} # Creates empty dict to store username and userUid pair to return
 	for page in user_response:
 		userz = page['users']
 		for user in userz:
 			all_users[user['username']] = user['userUid']
 	return all_users
 
-def no_account(department, all_users):
+def no_account(department, all_users): # Gets list of users in dept w/o account
 	return_val = []
 	for member in department:
 		if member not in all_users:
 			return_val.append(member)
 	return return_val
 
-def file_dia():
+def file_dia(): # TK file dialog 
 	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 	filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 	return filename
 
-def return_no_backup(users):
+def return_no_backup(users): # Returns users with no backup
 	return_value = []
 	for user,UID in users.items():
-		if len(get_machines(UID).items()) == 0:
+		if len(get_machines(UID).items()) == 0: # Count 0 lol
 			return_value.append(user)
 	return return_value
 
-def return_dept_dict(cp_all, dept_all):
+def return_dept_dict(cp_all, dept_all): # Returns dictionary with username/userUid combo for users w/ accounts in dept.
 	no_acc = no_account(dept_all, cp_all)	
 	return_value = {}
 	for member in dept_all:
@@ -109,7 +109,7 @@ def return_dept_dict(cp_all, dept_all):
 	return return_value
 
 
-def user_machine_status(UID):
+def user_machine_status(UID): # I could probably integrate return_no_backup() with this. #TODO
 	#print(f"Length: {len(get_machines(UID).items())}")
 	machine_data = get_machines(UID)
 	if len(machine_data.items()) != 0:
@@ -120,14 +120,14 @@ def user_machine_status(UID):
 	else:
 		print(f"User with UID {UID} does not have a machine associated with them.\n")
 
-def get_single_user(andrewID):
+def get_single_user(andrewID): # For one-off lookups.
 	andrewID = f"{andrewID}@andrew.cmu.edu"
 	cp_all_users = get_users()
 	print(f"The Status of {andrewID} is:")
 	return_value = user_machine_status(cp_all_users[andrewID])
 
-def sed(file, pattern, repl):
-	with open(file, "r") as sources:
+def sed(file, pattern, repl): # Sed-like replace function. Thank you to David Miller on Stack Overflow https://stackoverflow.com/questions/4427542/how-to-do-sed-like-text-replace-with-python
+	with open(file, "r") as sources: # I think I've done similar stuff in the past with nested withs lol. I didn't realize your variables were accessible outside of the with.
 		lines = sources.readlines()
 	with open(file, "w") as sources:
 		for line in lines:
